54a315e2d31ae702dc9a5c83296ad6039a08dbed
tony <tony.spectral@gmail.com>
2020-03-23 22:12:58 -0400

    publish

--- script/release.sh
+++ script/release.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/bash -x
 set -o nounset
 set -o errexit
 
 build=${BUILD_DIR:-build}
+doc=${DOC_DIR:-docs}
 image=${CR_IMAGE:-quay.io/helmpack/chart-releaser}
 ver=${CR_VERSION:-v1.0.0-beta.1}
 owner=${CR_OWNER:-the-watchmen}
@@ -10,30 +11,36 @@ repo=${CR_GIT_REPO:-helm-charts}
 
 echo "begin"
 
+echo "clearing ${build}"
+rm -rf ${build}
+
 for dir in charts/* ; do
   echo "packaging ${dir}"
   helm package -d $build $dir
 done
 
-echo "building index"
-helm repo index $build
-
-cmd="docker run \
+echo "running cr upload"
+docker run \
   --rm \
   -v $(pwd)/${build}:/${build} \
   -e CR_OWNER=${owner} \
   -e CR_GIT_REPO=${repo} \
   -e CR_PACKAGE_PATH=/${build} \
-  -e CR_INDEX_PATH=/${build}/index.yaml \
-  -e CR_CHARTS_REPO=https://${owner}.github.io/${repo} \
   -e CR_TOKEN=${CR_TOKEN} \
   ${image}:${ver} \
-  cr"
-
-echo "running cr upload"
-${cmd} upload
+  cr upload
 
 echo "running cr index"
-${cmd} index
+docker run \
+  --rm \
+  -v $(pwd)/${build}:/${build} \
+  -v $(pwd)/${doc}:/${doc} \
+  -e CR_OWNER=${owner} \
+  -e CR_GIT_REPO=${repo} \
+  -e CR_INDEX_PATH=/${doc}/index.yaml \
+  -e CR_CHARTS_REPO=https://${owner}.github.io/${repo} \
+  -e CR_PACKAGE_PATH=/${build} \
+  ${image}:${ver} \
+  cr index
 
 echo "end"
